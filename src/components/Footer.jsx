4e8cfe2c265867a9741a4e88999b967519cd0efa
SofiaFei <Feisofiafei@gmail.com>
2023-11-12 16:37:38 +0700

    feat: add components content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
+// images
+import Logo from '../assets/footer.svg';
+// icons
+import { FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa';
+// link
+import { Link } from 'react-scroll';
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+    return (
+        <footer className="h-fit bg-[#0A0314] w-full">
+            <div className="container mx-auto">
+                <div className="flex flex-col py-12 mt-28 text-center">
+                    {/* Logo */}
+                    <a href="#" className="flex justify-center ml-6"><img src={Logo} alt='' /></a>
+                    {/* permalinks */}
+                    <ul className="flex flex-col md:flex-row justify-center font-semibold gap-6 lg:gap-8 mx-auto my-12">
+                        <li><Link to='home' className="text-[#8F50BB] hover:text-white">Home</Link></li>
+                        <li><Link to='about' className="text-[#8F50BB] hover:text-white">About</Link></li>
+                        <li><Link to='services' className="text-[#8F50BB] hover:text-white">Services</Link></li>
+                        <li><Link to='skill' className="text-[#8F50BB] hover:text-white">Skills</Link></li>
+                        <li><Link to='work' className="text-[#8F50BB] hover:text-white">Project</Link></li>
+                        <li><Link to='contact' className="text-[#8F50BB] hover:text-white">Contact</Link></li>
+                    </ul>
+                    {/* social */}
+                    <div className="flex justify-center gap-4 mb-11 lg:mb-16">
+                        <a href='https://github.com/SofiaFei' className="bg-[#592A8F] text-white p-3 rounded-xl flex border-solid border border-transparent hover:bg-transparent hover:text-[#703DAB] hover:border-[#431D74]">
+                            <FaGithub />
+                        </a>
+                        <a href='https://www.instagram.com/soofyaaa__/' className="bg-[#8F50BB] text-white p-3 rounded-xl flex border-solid border border-transparent hover:bg-transparent hover:text-[#8F50BB] hover:border-[#8F50BB]">
+                            <FaInstagram />
+                        </a>
+                        <a href='https://www.linkedin.com/in/sofiafei/' className="bg-accent/90 text-white p-3 rounded-xl flex border-solid border border-transparent hover:bg-transparent hover:text-accent/90 hover:border-accent/90">
+                            <FaLinkedin />
+                        </a>
+                    </div>
+                    {/* copyright */}
+                    <div className="mb-16 text-white font-light">
+                        <small className="">&copy; Fei Sofia. All rights reserved.</small>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+};
 
-export default Footer
+export default Footer;
--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,9 +1,149 @@
-import React from 'react'
+import React from "react";
+// icon
+import { BsPatchCheckFill } from 'react-icons/bs';
+// motion
+import { motion } from 'framer-motion';
+// variant
+import { fadeIn } from '../variants';
 
 const Skill = () => {
-  return (
-    <div>Skill</div>
-  )
-}
+    return (
+        <section className="section" id='skill'>
+            <div className="container mx-auto">
+                <motion.div
+                    variants={fadeIn('right', 0.3)}
+                    initial="hidden"
+                    whileInView={'show'}
+                    viewport={{ once: false, amount: 0.3 }}>
+                    <h2 className="h2 leading-tight text-accent text-center mb-3">My Experience</h2>
+                    <h3 className="h3 text-center mb-16">What Skills I Have</h3>
+                </motion.div>
 
-export default Skill
+                <div className="flex flex-col lg:flex-row justify-center gap-8 sm:gap-4">
+                    <motion.div 
+                    variants={fadeIn('right', 0.5)}
+                    initial="hidden"
+                    whileInView={'show'}
+                    viewport={{ once: false, amount: 0.3 }}
+                    className="sm:w-11/12 w-full md:w-4/5 sm:mx-auto bg-accent/50 hover:bg-accent/5 border-transparent hover:border-accent/50 border-solid border-[1px] rounded-[32px] px-4 lg:px-20 py-8 lg:py-[38px] md:p-8">
+                        <h3 className="text-center text-[20px] tracking-wider font-primary font-semibold mb-8">Frontend Development</h3>
+                        <div className="grid grid-cols-2 gap-y-8 gap-x-2 lg:gap-x-8 sm:p-2 md:p-4 ml-3 sm:ml-12 md:ml-10 lg:ml-2">
+                            <article className="flex gap-4">
+                                <div className="mt-[6px] text-purple-500">
+                                    <BsPatchCheckFill />
+                                </div>
+                                <div>
+                                    <h4 className="font-semibold ">HTML</h4>
+                                    <small className="font-light">Experienced</small>
+                                </div>
+                            </article>
+                            <article className="flex gap-4">
+                                <div className="mt-[6px] text-purple-500">
+                                    <BsPatchCheckFill />
+                                </div>
+                                <div>
+                                    <h4 className="font-semibold">CSS</h4>
+                                    <small className="font-light">Experienced</small>
+                                </div>
+                            </article>
+                            <article className="flex gap-4">
+                                <div className="mt-[6px] text-purple-500 ">
+                                    <BsPatchCheckFill />
+                                </div>
+                                <div>
+                                    <h4 className="font-semibold">JavaScript</h4>
+                                    <small className="font-light">Intermediate</small>
+                                </div>
+                            </article>
+                            <article className="flex gap-4">
+                                <div className="mt-[6px] text-purple-500">
+                                    <BsPatchCheckFill />
+                                </div>
+                                <div>
+                                    <h4 className="font-semibold">Bootstrap</h4>
+                                    <small className="font-light">Intermediate</small>
+                                </div>
+                            </article>
+                            <article className="flex gap-4">
+                                <div className="mt-[6px] text-purple-500">
+                                    <BsPatchCheckFill />
+                                </div>
+                                <div>
+                                    <h4 className="font-semibold">Tailwind</h4>
+                                    <small className="font-light">Experienced</small>
+                                </div>
+                            </article>
+                            <article className="flex gap-4">
+                                <div className="mt-[6px] text-purple-500">
+                                    <BsPatchCheckFill />
+                                </div>
+                                <div>
+                                    <h4 className="font-semibold">React</h4>
+                                    <small className="font-light">Basic</small>
+                                </div>
+                            </article>
+                        </div>
+                    </motion.div>
+
+                    <motion.div 
+                    variants={fadeIn('left', 0.5)}
+                    initial="hidden"
+                    whileInView={'show'}
+                    viewport={{ once: false, amount: 0.3 }}
+                    className="sm:w-11/12 w-full md:w-4/5 sm:mx-auto bg-accent/50 hover:bg-accent/5 border-transparent hover:border-accent/50 border-solid border-[1px] rounded-[32px] px-4 lg:px-20 py-8 lg:py-[38px] md:p-8">
+                        <h3 className="text-center text-[20px] tracking-wider font-primary font-semibold mb-8">Backend Development</h3>
+                        <div className="grid grid-cols-2 gap-y-8 gap-x-2 lg:gap-x-8 sm:p-2 md:p-4 ml-3 sm:ml-12 md:ml-10 lg:ml-2">
+                            <article className="flex gap-4">
+                                <div className="mt-[6px] text-purple-500">
+                                    <BsPatchCheckFill />
+                                </div>
+                                <div>
+                                    <h4 className="font-semibold">NodeJS</h4>
+                                    <small className="font-light">Intermediate</small>
+                                </div>
+                            </article>
+                            <article className="flex gap-4">
+                                <div className="mt-[6px] text-purple-500">
+                                    <BsPatchCheckFill />
+                                </div>
+                                <div>
+                                    <h4 className="font-semibold">PHP</h4>
+                                    <small className="font-light">Intermediate</small>
+                                </div>
+                            </article>
+                            <article className="flex gap-4">
+                                <div className="mt-[6px] text-purple-500">
+                                    <BsPatchCheckFill />
+                                </div>
+                                <div>
+                                    <h4 className="font-semibold">MySQL</h4>
+                                    <small className="font-light">Experienced</small>
+                                </div>
+                            </article>
+                            <article className="flex gap-4">
+                                <div className="mt-[6px] text-purple-500">
+                                    <BsPatchCheckFill />
+                                </div>
+                                <div>
+                                    <h4 className="font-semibold">Python</h4>
+                                    <small className="font-light">Intermediate</small>
+                                </div>
+                            </article>
+                            <article className="flex gap-4">
+                                <div className="mt-[6px] text-purple-500">
+                                    <BsPatchCheckFill />
+                                </div>
+                                <div>
+                                    <h4 className="font-semibold">Laravel</h4>
+                                    <small className="font-light">Experienced</small>
+                                </div>
+                            </article>
+                        </div>
+                    </motion.div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Skill;
